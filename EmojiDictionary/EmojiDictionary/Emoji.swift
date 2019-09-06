24ca8e7af9589bfd49eb419c947ac4c76f765b5b
gracefulcanada@gmail.com <gracefulcanada@gmail.com>
2019-09-06 07:35:19 -0700

    Home furniture

---
+++ EmojiDictionary/EmojiDictionary/Emoji.swift
@@ -0,0 +1,15 @@
+
+import Foundation
+
+struct Emoji: Codable {
+    var symbol: String
+    var name: String
+    var detailDescription: String
+    var usage: String
+    
+    static func saveToFile(emojis:){
+        
+    }
+}
+
+let documentsDiretory
